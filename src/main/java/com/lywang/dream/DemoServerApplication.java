9d60c9662fd727e12c1d1842f59a4de9fa24e936
lywang <lywang@nltechdev.com>
2020-03-28 22:56:30 +0800

    create spring boot project

---
+++ src/main/java/com/lywang/dream/DemoServerApplication.java
@@ -0,0 +1,13 @@
+package com.lywang.dream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreamServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/examplcom/dream/DemoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examplcom.dream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
